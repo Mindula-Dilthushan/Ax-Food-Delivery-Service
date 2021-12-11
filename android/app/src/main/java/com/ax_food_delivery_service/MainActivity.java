fe47fd52daff5c7826f9869e4ab435845b53d847
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-12-11 12:13:58 +0530

    project init

---
+++ android/app/src/main/java/com/ax_food_delivery_service/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ax_food_delivery_service;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Ax_Food_Delivery_Service";
+  }
+}
